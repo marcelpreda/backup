d82d8ded385ea4eb9124ec4fa19aa9b95f0272ce
Marcel Preda <marcel_preda@yahoo.com>
2024-01-25 23:57:05 +0200

    powershell script to zip folders/files and copy them over ssh on NAS

---
+++ power_shell/backup_pfa.ps1
@@ -0,0 +1,49 @@
+$backup_host = "nsa320"
+$backup_user = "backup"
+$src_path = @("D:\test_backup", "D:\kit", "D:\a.js", "D:\Workfolder test")
+$date = Get-Date -Format "yyyy_MM_dd__HH_mm_ss"
+$backup_dst_path = "/i-data/1201a80c/backup/$Env:USERNAME/$date"
+
+
+ssh ${backup_user}@${backup_host} "mkdir -p $backup_dst_path"
+
+# create the local folder for archiving
+$new_bkp_path = New-Item -Path "$pwd\backup_$date" -ItemType Directory
+
+# Copress $src to $dst
+Function CompressItem($src, $dst) {
+    $compress = @{
+        Path = $src
+        DestinationPath = $dst
+        CompressionLevel = "Fastest"
+    }
+    Compress-Archive @compress
+    #return $compress.DestinationPath
+}
+
+
+# keep here all the scp processes tahat are started in background
+# later we should wait for them to finish
+$all_scp_procs = @()
+
+foreach ($p in $src_path) {
+    $f_name = Split-Path $p -leaf
+    $zip_name = "$f_name.zip".Replace(" ", "_")
+    $dst_zip = "$new_bkp_path\$zip_name"
+    Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    "    Compress $p"
+    "    To       $dst_zip"
+    CompressItem $p $dst_zip        
+    $proc = Start-Process scp -ArgumentList "$dst_zip ${backup_user}@${backup_host}:${backup_dst_path}" -PassThru
+    $all_scp_procs += $proc
+}
+
+Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+"    All archives created. Wait to finish all scp processes"
+
+Foreach ($p in $all_scp_procs) {
+    $p.WaitForExit()
+}
+Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+Read-Host -Prompt "Press any key and then ENTER to close." | Out-Null
+
